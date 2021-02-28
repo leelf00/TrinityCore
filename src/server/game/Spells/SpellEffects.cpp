852a1273b004f6630580435da7867c9116cd2d9d
Robingad <robingad@rambler.ru>
2021-02-28 19:10:27 +0200

    Core/Spells: Add nullptr itemTarget check in Spell::EffectEnchantItemTmp (#26172)
    (cherry picked from commit 5e78494e541b15ede4ca41c92654d6c401f6dc4c)

--- src/server/game/Spells/SpellEffects.cpp
+++ src/server/game/Spells/SpellEffects.cpp
@@ -2475,6 +2475,9 @@ void Spell::EffectEnchantItemTmp(SpellEffIndex effIndex)
     if (effectHandleMode != SPELL_EFFECT_HANDLE_HIT_TARGET)
         return;
 
+    if (!itemTarget)
+        return;
+
     Player* player = m_caster->ToPlayer();
     if (!player)
         return;
