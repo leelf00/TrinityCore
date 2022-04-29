57a307f83fb3589b902002a2b56332259b9f98c6
TheGhost <theghostgroup@gmail.com>
2022-04-29 19:09:04 +1200

    Core/Script: Add early concept shadowmoon valley script & remove script from draenor script loader.
    also fix build.

--- src/server/scripts/Draenor/dreanor_script_loader.cpp
+++ src/server/scripts/Draenor/dreanor_script_loader.cpp
@@ -16,11 +16,11 @@
 */
 
 // This is where scripts' loading functions should be declared:
-void AddSC_draenor_shadowmoon_valley();
+// void AddSC_draenor_shadowmoon_valley();
 
 // The name of this function should match:
 // void Add${NameOfDirectory}Scripts()
 void AddDraenorScripts()
 {
-  AddSC_draenor_shadowmoon_valley();
+  // AddSC_draenor_shadowmoon_valley();
 }
