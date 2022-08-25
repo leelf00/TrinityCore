cff7fe5221af6249f0c46cb2fdb5cdc959dd743e
TheGhostGroup <theghostgroup@gmail.com>
2022-08-25 18:25:19 +1200

    DB/Creature: Godan, Enchanting Trainer

---
+++ sql/custom/world/2022_08_25_06_World.sql
@@ -0,0 +1,3 @@
+-- Godan, Enchanting Trainer
+UPDATE `creature_template` SET `npcflag` = 83 WHERE `entry` = 3345;
+INSERT INTO `gossip_menu_option_trainer` (`MenuId`, `OptionIndex`, `TrainerId`) VALUES (4168, 0, 62);
