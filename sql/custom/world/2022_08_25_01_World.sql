faf753e995f1cd10b8fe97463e5c2569d7fe3973
TheGhostGroup <theghostgroup@gmail.com>
2022-08-25 17:57:15 +1200

    DB/Creature: Arathel Sunforge, Blacksmithing Trainer.

---
+++ sql/custom/world/2022_08_25_01_World.sql
@@ -0,0 +1,3 @@
+-- Arathel Sunforge, Blacksmithing Trainer.
+DELETE FROM `gossip_menu_option_trainer` WHERE `MenuID` = 7256;
+INSERT INTO `gossip_menu_option_trainer` (`MenuId`, `OptionIndex`, `TrainerId`) VALUES (7256, 0, 27);
