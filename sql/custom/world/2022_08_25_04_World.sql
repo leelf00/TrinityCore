fd3c6538d30ff876c3561553591ffe6095b11278
TheGhostGroup <theghostgroup@gmail.com>
2022-08-25 18:04:39 +1200

    DB/Creature: Engineer Palehoof, Engineering Trainer.

---
+++ sql/custom/world/2022_08_25_04_World.sql
@@ -0,0 +1,3 @@
+-- Engineer Palehoof, Engineering Trainer
+UPDATE `creature_template` SET `npcflag` = 83 WHERE `entry` = 52651;
+INSERT INTO `gossip_menu_option_trainer` (`MenuId`, `OptionIndex`, `TrainerId`) VALUES (13014, 0, 407);
