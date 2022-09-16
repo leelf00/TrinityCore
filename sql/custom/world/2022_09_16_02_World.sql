d5dcb1f5f53ebc8b5eb4cc85acad3d977b059e29
TheGhostGroup <theghostgroup@gmail.com>
2022-09-16 12:55:15 +1200

    DB/Gossip:  Shelene Rhobart, Leatherworking Trainer

---
+++ sql/custom/world/2022_09_16_02_World.sql
@@ -0,0 +1,3 @@
+-- Shelene Rhobart, Leatherworking Trainer *
+DELETE FROM `gossip_menu_option_trainer` WHERE `MenuID` = 4184;
+INSERT INTO `gossip_menu_option_trainer` (`MenuId`, `OptionIndex`, `TrainerId`) VALUES (4184, 0, 56);
