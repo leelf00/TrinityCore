581344cb39c5d54f0d546c1b0b892323325f77cf
TheGhostGroup <theghostgroup@gmail.com>
2022-09-20 15:28:00 +1200

    DB/World: Add templates data for character templates.

---
+++ sql/custom/world/2022_09_20_05_World.sql
@@ -0,0 +1,33 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               8.0.30 - MySQL Community Server - GPL
+-- Server OS:                    Win64
+-- HeidiSQL Version:             12.0.0.6468
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+-- Dumping data for table converted_spp.character_template: ~9 rows (approximately)
+INSERT INTO `character_template` (`ID`, `Name`, `Description`, `Level`) VALUES
+	(37, '100 level character', 'Create character with 100 level and 640ilvl items', 100),
+	(38, '110 level character', 'Create character with 110 level and 810ilvl items', 110),
+	(39, '110 level 845 ilevel', 'Create character with 110 level and 845ilvl items', 110),
+	(40, '110 level 865 ilevel', 'Create character with 110 level and 865ilvl items', 110),
+	(41, '110 level 895 ilevel', 'Create character with 110 level and 895ilvl items', 110),
+	(42, '110 level 925 ilevel', 'Create character with 110 level and 925ilvl items', 110),
+	(43, '110 level 945 ilevel', 'Create character with 110 level and 945ilvl items', 110),
+	(44, '110 level 965 ilevel', 'Create character with 110 level and 965ilvl items', 110),
+	(45, '110 level 985 ilevel', 'Create character with 110 level and 985ilvl items', 110);
+
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
---
+++ sql/custom/world/2022_09_20_06_World.sql
@@ -0,0 +1,72 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               8.0.30 - MySQL Community Server - GPL
+-- Server OS:                    Win64
+-- HeidiSQL Version:             12.0.0.6468
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+-- Dumping data for table converted_spp.character_template_class: ~48 rows (approximately)
+INSERT INTO `character_template_class` (`TemplateId`, `FactionGroup`, `Class`) VALUES
+	(37, 3, 1),
+	(37, 3, 2),
+	(37, 3, 3),
+	(37, 3, 4),
+	(37, 3, 5),
+	(37, 3, 6),
+	(37, 3, 7),
+	(37, 3, 8),
+	(37, 3, 9),
+	(37, 3, 10),
+	(37, 3, 11),
+	(37, 3, 12),
+	(37, 5, 1),
+	(37, 5, 2),
+	(37, 5, 3),
+	(37, 5, 4),
+	(37, 5, 5),
+	(37, 5, 6),
+	(37, 5, 7),
+	(37, 5, 8),
+	(37, 5, 9),
+	(37, 5, 10),
+	(37, 5, 11),
+	(37, 5, 12),
+	(38, 3, 1),
+	(38, 3, 2),
+	(38, 3, 3),
+	(38, 3, 4),
+	(38, 3, 5),
+	(38, 3, 6),
+	(38, 3, 7),
+	(38, 3, 8),
+	(38, 3, 9),
+	(38, 3, 10),
+	(38, 3, 11),
+	(38, 3, 12),
+	(38, 5, 1),
+	(38, 5, 2),
+	(38, 5, 3),
+	(38, 5, 4),
+	(38, 5, 5),
+	(38, 5, 6),
+	(38, 5, 7),
+	(38, 5, 8),
+	(38, 5, 9),
+	(38, 5, 10),
+	(38, 5, 11),
+	(38, 5, 12);
+
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
