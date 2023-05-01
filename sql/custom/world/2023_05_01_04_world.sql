2a0ea0d620f1a067eda33a874b0437d2ae2c299e
TheGhostGroup <theghostgroup@gmail.com>
2023-05-01 17:49:08 +1200

    Core/Spell: Add warrior jump to skyhold.

---
+++ sql/custom/world/2023_05_01_04_world.sql
@@ -0,0 +1,4 @@
+-- spell warrior jump
+DELETE FROM `spell_script_names` WHERE `spell_id` = 192085 AND `ScriptName`='spell_warr_jump_to_skyhold';
+INSERT INTO spell_script_names VALUES 
+(192085, "spell_warr_jump_to_skyhold");
