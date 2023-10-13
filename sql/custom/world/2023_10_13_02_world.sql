1648fff856a7478568a98eb20aeae2f81622cc19
TheGhostGroup <theghostgroup@gmail.com>
2023-10-13 21:37:58 +1300

    DB/Misc: fix query.

--- sql/custom/world/2023_10_13_02_world.sql
+++ sql/custom/world/2023_10_13_02_world.sql
@@ -8,4 +8,4 @@ DELETE FROM `gossip_menu_option` WHERE `MenuId`=20486 AND `OptionIndex`=0;
 INSERT INTO `gossip_menu_option` (`MenuId`, `OptionIndex`, `OptionIcon`, `OptionText`, `OptionBroadcastTextId`, `OptionType`, `OptionNpcflag`, `VerifiedBuild`) VALUES 
 (20486, 0, 0, 'I already heard that ... <Skip the introductory assignments of the Legion and begin the journey to Dalaran.>', 123132, 1, 1, 26972);
 
-UPDATE `creature_template` SET `npcflag`=`npcflag`|1, `gossip_menu_id`=20486 WHERE `entry`=107934;
+UPDATE `creature_template` SET `npcflag`=`npcflag`|1|2, `gossip_menu_id`=20486 WHERE `entry`=107934;
