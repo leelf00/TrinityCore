b293e205e5a01d8c65d9ab351d34c3f616882860
TheGhostGroup <theghostgroup@gmail.com>
2023-10-15 14:41:43 +1300

    DB/Misc: fix my error.
    * Wrong table.

--- sql/custom/world/2023_10_13_01_world.sql
+++ sql/custom/world/2023_10_13_01_world.sql
@@ -1,2 +1,2 @@
 -- misc fixes
-UPDATE `game_event_npc_vendor` SET `PlayerConditionID`=0 WHERE `PlayerConditionID` IN (5201, 13632, 13633, 12250, 12249, 19985);
+UPDATE `npc_vendor` SET `PlayerConditionID`=0 WHERE `PlayerConditionID` IN (5201, 13632, 13633, 12250, 12249, 19985);
